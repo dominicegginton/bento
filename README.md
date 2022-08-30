815e935a1ce4f33c78e2a5046c462ec01ba2b210
Dominic Egginton <dominic.egginton@gmail.com>
2022-08-30 14:24:36 +0100

    chore(workspace): initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bento
+
+![banner](https://user-images.githubusercontent.com/28626241/135669201-0d2e7e9a-0a71-4c68-93b7-4a510e464f00.png)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { map, interval } from 'rxjs';
+
+@Component({
+  selector: 'bento-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  time$ = interval(100).pipe(map(() => new Date()));
+  greeting$ = interval(100).pipe(map(() => this.greeting()));
+
+  private greeting() {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) return 'Good Morning ☕️';
+    if (hour >= 12 && hour < 18) return 'Good Afternoon ☀️';
+    if (hour >= 18 && hour < 22) return 'Good Evening 🌃';
+    if (hour >= 22 || hour < 5) return 'Goodnight 🌙';
+    return 'Have a good day 😄';
+  }
+}
